6fe149ea5fe00cf2a72b0c8caa80e0943d1d52bc
Hyeongseok Lee <tony@Hyeongseoks-MacBook-Pro.local>
2022-06-28 11:02:59 -0400

    Hi added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, world! This is Tony.</Text>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
     </View>
   );
