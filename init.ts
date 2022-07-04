e512f136a4b2d33af701d29c00107549a8c16775
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-07-04 15:11:26 -0400

    feat: init db on app mount

---
+++ init.ts
@@ -0,0 +1,62 @@
+import { nanoid } from 'nanoid';
+import { addParticipant, createActivity, register } from './backend';
+
+async function initUsers() {
+  const userAndrew = await register({
+    name: 'Andrew',
+    bio: 'Hello my name is Andrew',
+    password: 'Abcd@1234',
+  });
+
+  const userTony = await register({
+    name: 'Tony',
+    bio: 'Hello my name is Tony',
+    password: 'Abcd@1234',
+  });
+
+  const userThomas = await register({
+    name: 'Thomas',
+    bio: 'Hello my name is Thomas',
+    password: 'Abcd@1234',
+  });
+
+  return { userAndrew, userTony, userThomas };
+}
+
+export async function init() {
+  const { userAndrew, userThomas, userTony } = await initUsers();
+
+  const biking = await createActivity({
+    city: 'Toronto',
+    description: "Let's go biking",
+    googleMapUrl: 'https://goo.gl/maps/xuueMQhnKxmWBEfj8',
+    limit: 2,
+    startDate: '2023-01-01',
+    title: 'Biking',
+    userId: userAndrew.id,
+  });
+
+  const coffee = await createActivity({
+    city: 'Toronto',
+    description: "Let's go cafe",
+    googleMapUrl: 'https://goo.gl/maps/wXm7SdkH14ji8pkJA',
+    limit: 20,
+    startDate: '2022-08-01',
+    title: 'Coffee',
+    userId: userTony.id,
+  });
+
+  const beer = await createActivity({
+    city: 'Toronto',
+    description: "Let's go beer",
+    googleMapUrl: 'https://goo.gl/maps/UEgaieYosqw4doKL7',
+    limit: 100,
+    startDate: '2023-01-01',
+    title: 'Beer',
+    userId: userThomas.id,
+  });
+
+  await addParticipant(beer.id, userAndrew);
+  await addParticipant(beer.id, userTony);
+  await addParticipant(coffee.id, userThomas);
+}
