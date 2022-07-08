00d302015c1ade1ec03480513d3032838dcce859
Andrew Nguyen <52666982+Andrewnt219@users.noreply.github.com>
2022-07-08 10:55:31 -0400

    feat: display (you) participants

--- components/ParticipantListItem/index.tsx
+++ components/ParticipantListItem/index.tsx
@@ -1,15 +1,23 @@
 import { Layout, Text } from '@ui-kitten/components';
 import React from 'react';
 import { Participant } from '../../backend';
+import { useAuthUserContext } from '../../context/AuthUserContext';
 
 export interface ParticipantListItemProps {
   participant: Participant;
 }
 
 export const ParticipantListItem = (props: ParticipantListItemProps) => {
+  const { authUser } = useAuthUserContext();
   return (
-    <Layout>
+    <Layout style={{ flexDirection: 'row' }}>
       <Text>{props.participant.name}</Text>
+
+      {authUser?.id === props.participant.id && (
+        <Text status="info" style={{ marginLeft: 4 }}>
+          (You)
+        </Text>
+      )}
     </Layout>
   );
 };
